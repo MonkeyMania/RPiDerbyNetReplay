6664bd26c9f32b81f3c3823fe36884ccdaa55b74
MonkeyMania <46270068+MonkeyMania@users.noreply.github.com>
2018-12-31 12:15:29 -0500

    tab/space fixing
    climbing out of a hole of python tab vs. spaces and number of spaces royal confusion - Thank you Notepad++ for showing tabs vs. spaces

--- RPiDerbyNetReplay.py
+++ RPiDerbyNetReplay.py
@@ -37,121 +37,122 @@ stream = picamera.PiCameraCircularIO(camera, seconds=7)
 dirpref = time.strftime("%Y-%m-%d recordings")
 directory = dirpref + "/"
 if not os.path.exists(directory):
-        os.makedirs(directory)
+    os.makedirs(directory)
 ################ END CREATE VIDEO DIRECTORY ################
 
 ################ START FUNCTIONS ################
 # function for checking in with the server - here for cleanliness of main code
 def ReplayCheckIn(strURL, strData):
-        r = requests.post(url = strURL, data = strData)
+    r = requests.post(url = strURL, data = strData)
 
-        tree = ElementTree.fromstring(r.content)
+    tree = ElementTree.fromstring(r.content)
 
-        gotthemessage = False
-        for elem in tree.iter():
-                if elem.tag == "replay-message":
-                        replaymessage = elem.text.split(" ")
-                        gotthemessage = True
-        if gotthemessage:
-                return replaymessage
-        else:
-                return "NORESPONSE"
+    gotthemessage = False
+    for elem in tree.iter():
+        if elem.tag == "replay-message":
+            replaymessage = elem.text.split(" ")
+            gotthemessage = True
+    if gotthemessage:
+        return replaymessage
+    else:
+        return "NORESPONSE"
 
 # function for toggling the screen to be blanked or not - here due to repeated use      
 def ScreenBlanked(toggle = True):
-        if toggle == True:
-                #set blank screen behind everything
-                pygame.display.init()
-                screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-                screen.fill((0, 0, 0))
-        else:
-                pygame.display.quit()
+    if toggle == True:
+        #set blank screen behind everything
+        pygame.display.init()
+        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        screen.fill((0, 0, 0))
+    else:
+        pygame.display.quit()
 
 ################ END FUNCTIONS ################
+################ START MAIN ROUTINE ################
 
 # Setup last checkin to ensure triggered on first run
 LastCheckIn = time.time() - 30
 
 try:
-        while True:
-                curTime = time.time()
-                #is it time to check in?
-                if curTime - LastCheckIn > checkInInt:
-                        Replayparams = {'action':'replay-message', 'status':Pstatus, 'finished-replay':PreplayFin}
-                        responseList = ReplayCheckIn(replayurl, Replayparams)
-                        # Reset replayfin after last POST to only send one scan of 1
-                        PreplayFin = 0
-                        LastCheckIn = curTime
-                        print("checked in",responseList)
-            
-                        if responseList[0] == "HELLO":
-                                Pstatus = 0
-                                print("Hello!")
-                        if responseList[0] == "TEST":
-                                # Ensure we only do something when nothing else was happening
-                                if Pstatus == 0:
-                                        Pstatus = 2
-                                        print("Testing!"," Skipback=",responseList[1]," Showings=",responseList[2]," Rate=",responseList[3])
-                                        ScreenBlanked(True)
-                                        filemp4 = testfilename
-                                        replaycount = 0
-                                        showings = int(responseList[2])
-                                        playbackstarted = False
-                                        playbackduration = 15
-                        if responseList[0] == "START":
-                                Pstatus = 1
-                                ScreenBlanked(True)
-                                filemp4 = directory + time.strftime("%H%M%s_") + responseList[1] + ".mp4"
-                                filename = directory + time.strftime("%H%M%s_") + responseList[1] + ".h264"
-                                camera.start_recording(stream, format='h264', intra_period = 10)
-                                camera.start_preview()
-                        if responseList[0] == "REPLAY":
-                                # only do something if we get this while recording
-                                # which also means we should have filenames defined
-                                if Pstatus == 1:
-                                        Pstatus = 2
-                                        # Save requested last few seconds
-                                        # I'm assuming here that REPLAY comes AFTER last car crosses
-                                        stream.copy_to(filename, seconds=responseList[1])
-                                        # playback with h264 is wonky, convert to mp4
-                                        convertstring = "MP4Box -fps " + str(thisframerate * int(responseList[3])) + " -add " + filename + " " + filemp4
-                                        os.system(convertstring)
-                                        camera.stop_preview()
-                                        replaycount = 0
-                                        showings = int(responseList[2])
-                                        playbackstarted = False
-                                        # Estimate and pad playback time
-                                        playbackduration = int(responseList[1]) / int(responseList[3]) + 2
-                        if responseList[0] == "CANCEL":
-                                Pstatus = 0
-                                camera.stop_recording()
-                                camera.stop_preview()
-                                ScreenBlanked(False)
-                        if responseList[0] == "NORESPONSE":
-                                print("No updates")
-        
-                # We're in playback mode
-                if Pstatus == 2:
-                        # Are we currently playing back?
-                        if playbackstarted == True:
-                                # Is the last playback expected complete?
-                                if curTime - playbackstart > playbackduration:
-                                        playbackstarted = False
-                                        replaycount = replaycount + 1
-                                        # Was this the last one?
-                                        if replaycount >= showings:
-                                                PreplayFin = 1
-                                                Pstatus = 0
-                                                ScreenBlanked(False)
-                        else:
-                                # start a playback (playback is false, buqt we're here)
-                                omxc = Popen(['omxplayer', filemp4])
-                                playbackstart = curTime
-                                playbackstarted = True
-       
+    while True:
+        curTime = time.time()
+        #is it time to check in?
+        if curTime - LastCheckIn > checkInInt:
+            Replayparams = {'action':'replay-message', 'status':Pstatus, 'finished-replay':PreplayFin}
+            responseList = ReplayCheckIn(replayurl, Replayparams)
+            # Reset replayfin after last POST to only send one scan of 1
+            PreplayFin = 0
+            LastCheckIn = curTime
+            print("checked in",responseList)
+
+            if responseList[0] == "HELLO":
+                Pstatus = 0
+                print("Hello!")
+            if responseList[0] == "TEST":
+                # Ensure we only do something when nothing else was happening
+                if Pstatus == 0:
+                    Pstatus = 2
+                    print("Testing!"," Skipback=",responseList[1]," Showings=",responseList[2]," Rate=",responseList[3])
+                    ScreenBlanked(True)
+                    filemp4 = testfilename
+                    replaycount = 0
+                    showings = int(responseList[2])
+                    playbackstarted = False
+                    playbackduration = 15
+            if responseList[0] == "START":
+                Pstatus = 1
+                ScreenBlanked(True)
+                filemp4 = directory + time.strftime("%H%M%s_") + responseList[1] + ".mp4"
+                filename = directory + time.strftime("%H%M%s_") + responseList[1] + ".h264"
+                camera.start_recording(stream, format='h264', intra_period = 10)
+                camera.start_preview()
+            if responseList[0] == "REPLAY":
+                # only do something if we get this while recording
+                # which also means we should have filenames defined
+                if Pstatus == 1:
+                    Pstatus = 2
+                    # Save requested last few seconds
+                    # I'm assuming here that REPLAY comes AFTER last car crosses
+                    stream.copy_to(filename, seconds=responseList[1])
+                    # playback with h264 is wonky, convert to mp4
+                    convertstring = "MP4Box -fps " + str(thisframerate * int(responseList[3])) + " -add " + filename + " " + filemp4
+                    os.system(convertstring)
+                    camera.stop_preview()
+                    replaycount = 0
+                    showings = int(responseList[2])
+                    playbackstarted = False
+                    # Estimate and pad playback time
+                    playbackduration = int(responseList[1]) / int(responseList[3]) + 2
+            if responseList[0] == "CANCEL":
+                Pstatus = 0
+                camera.stop_recording()
+                camera.stop_preview()
+                ScreenBlanked(False)
+            if responseList[0] == "NORESPONSE":
+                print("No updates")
+
+        # We're in playback mode
+        if Pstatus == 2:
+        # Are we currently playing back?
+        if playbackstarted == True:
+            # Is the last playback expected complete?
+            if curTime - playbackstart > playbackduration:
+                playbackstarted = False
+                replaycount = replaycount + 1
+                # Was this the last one?
+                if replaycount >= showings:
+                    PreplayFin = 1
+                    Pstatus = 0
+                    ScreenBlanked(False)
+        else:
+            # start a playback (playback is false, but we're in playback status)
+            omxc = Popen(['omxplayer', filemp4])
+            playbackstart = curTime
+            playbackstarted = True
+
 finally:
-        pygame.display.quit()
-        camera.stop_recording()
-        camera.stop_preview()
-        camera.close()
-        sys.exit()
+    pygame.display.quit()
+    camera.stop_recording()
+    camera.stop_preview()
+    camera.close()
+    sys.exit()
